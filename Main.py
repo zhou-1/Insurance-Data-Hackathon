0e6f084f517a673e4a52fb2d2ec0f3f51f826df0
Zhou <zhou.shen@marquette.edu>
2018-04-06 22:26:43 -0500

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,13 @@
+# Predict how many Financial Representatives will remain in the profession.
+"""
+Ideas:
+Apply the first 5 years data for data consistency.  
+Predict the situation in next 2 years (6th and 7th) 
+Improve the accuracy of prediction
+
+Steps:
+Data Preprocessing 
+Data Extraction
+Support Vector Machine Classification
+Data Test
+"""
